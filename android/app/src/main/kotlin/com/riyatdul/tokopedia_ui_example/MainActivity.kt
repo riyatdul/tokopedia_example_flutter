be632cfa54110bb5ef49170637238aeed5ae304b
riyatdul <alriyatdul@gmail.com>
2021-11-29 01:05:35 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/riyatdul/tokopedia_ui_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.riyatdul.tokopedia_ui_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:tokopedia_ui_example/ui/ui.dart';
+
+part 'custom_bottom_navbar.dart';
